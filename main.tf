b9dc4f0c9b5c177b8033fd284855ee8e8c61926e
Jonathan Wong <jonathanqwong@gmail.com>
2023-08-10 09:51:30 -0700

    create dev vpc patch

--- main.tf
+++ main.tf
@@ -14,14 +14,14 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west2b"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
 
@@ -45,7 +45,7 @@ module "blog_security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
   name    = "blog_new"
-  vpc_id  = module.vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
