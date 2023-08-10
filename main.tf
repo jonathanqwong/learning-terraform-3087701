2149cf5b51fea5a16b4cf5d87a10c7796509c25d
Jonathan Wong <jonathanqwong@gmail.com>
2023-08-09 20:52:40 -0700

    use module for security

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_security_group.security_group_id]
 
   tags = {
     Name = "HelloWorld"
@@ -27,6 +27,20 @@ resource "aws_instance" "blog" {
 
 data "aws_vpc" "default" {
   default = true
+}
+
+module "blog_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog_new"
+
+  vpc_id              = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
